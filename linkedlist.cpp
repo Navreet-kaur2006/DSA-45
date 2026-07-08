2024e4bb28e87527c39eb5f7744f539a0208ae6b
navreet <navreetkaur732006@gmail.com>
2026-07-08 21:06:13 +0530

    list

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,15 +1,367 @@
 //LINKED LIST
-#include<iostream>
+
+ /* #include<iostream>
+#include<vector>                       // only pushfront 
 using namespace std;
 
-class node(){
+class Node{                // to create node
+   public:
+    int data;
+   Node* next;                 // store address of next node
+  
+   Node(int val){
+    data=val;
+    next=NULL;
+   }
+
+
+};
+
+class Linkedlist{  
+    public:            // to  create linked list
+    Node* head;
+    Node* tail;
+
+    Linkedlist(){
+
+        head=NULL;
+        tail=NULL;
+    
+
+    }
+
+
+    void push_front(int val){
+            Node* newNode = new Node(val); 
+
+    if(head==NULL && tail==NULL){
+        tail=head=newNode;
+        }
+        else{
+            newNode->next=head;
+            head=newNode;
+
+        }
+
+
+
+    }
+
+    void print_LL(){
+        Node* temp=head;
+        while(temp !=NULL){
+            cout<<temp->data<<"-> ";
+            temp=temp-> next;
+        }
+        cout<<"NULL"<<endl;
+
+    
+    }
 
 
 };
 
 
 
+
 int main(){
+   
+   vector<int>nums;
+   Linkedlist ll;
+
+   ll.push_front(6);
+   ll.push_front(8);
+   ll.push_front(9);
+   ll.push_front(10);
+
+   ll.print_LL();       
+   
+
+}
+ */  
 
 
-}  
+
+
+/*   #include<iostream>
+#include<vector>                                             
+using namespace std;                        
+
+class Node{                // to create node
+   public:
+    int data;
+   Node* next;                 // store address of next node
+  
+   Node(int val){
+    data=val;
+    next=NULL;
+   }
+
+
+};
+
+class Linkedlist{  
+    public:            // to  create linked list
+    Node* head;
+    Node* tail;
+
+    Linkedlist(){
+
+        head=NULL;
+        tail=NULL;
+    
+
+    }
+
+    void push_front(int val){
+            Node* newNode = new Node(val);    // for new node
+
+    if(head==NULL && tail==NULL){
+        tail=head=newNode;
+        }
+        else{
+            newNode->next=head;
+            head=newNode;
+
+        }
+
+    }
+
+    void pop_front(){                          // to delete the node from the front
+      
+        if(head==NULL&& tail==NULL){
+            cout<<"linked list is empty";
+            return;
+        }
+        else{
+            Node* temp=head;
+            head=temp->next;
+            temp->next=NULL;
+            delete temp;
+
+        }
+    }
+
+
+
+    void pop_back(){                        // to delete node from back
+        if(head==NULL && tail==NULL){
+            cout<<"linked list is empty";
+            return;
+        }
+        else{
+            Node* temp=head;
+
+            while(temp->next->next!=NULL){
+
+                    temp=temp->next; 
+            }
+
+
+           temp->next=NULL;
+           delete tail;
+
+           tail=temp;
+        }
+
+    }
+
+
+
+
+    void push_back(int val){
+
+        Node* newNode = new Node(val); 
+
+    if(head==NULL && tail==NULL){
+        tail=head=newNode;
+        }
+        else{
+
+            tail->next = newNode;
+            tail = newNode; 
+
+        }
+
+
+
+
+    } 
+
+
+    void insert(int val,int pos){                       
+         Node* newNode = new Node(val);                  // to insert a new node
+         
+    if(pos<=0){
+            cout<<"invalid position";
+         }
+         else if(pos==1){
+            push_front(val);
+         }
+    else{
+        Node* temp=head;
+            for(int i=1;i<pos-1;i++){
+
+            if(temp==NULL){
+                return;
+            }
+                temp=temp->next;
+
+            }
+
+            Node* newNode=new Node(val);
+
+            newNode->next=temp->next;
+            temp->next=newNode;
+
+
+         }
+    }
+
+
+
+
+    void print_LL(){
+        Node* temp=head;
+        while(temp !=NULL){
+            cout<<temp->data<<"-> ";
+            temp=temp->next;
+        }
+        cout<<"NULL"<<endl;
+
+    
+    }
+
+
+};
+
+
+
+
+int main(){
+   
+   vector<int>nums;
+   Linkedlist ll;
+
+
+
+   ll.push_back(6);
+   ll.push_back(9);
+   ll.push_front(4);
+   ll.insert(7,2);
+
+   //ll.pop_front();
+  ll.pop_back();
+
+   ll.print_LL();       
+}     
+   */
+
+
+
+
+   //DOUBLY LINKED LIST
+
+ #include<iostream>
+#include<vector>                                             
+using namespace std;                        
+
+class Node{                // to create node
+   public:
+    int data;
+   Node* next;                  // store address of previous  node
+   Node* prev;                 // store address of next node
+  
+   Node(int val){
+    data=val;
+    prev=NULL;
+    next=NULL;
+   }
+
+
+};
+
+class doublylinkedlist{  
+    public:            // to  create linked list
+    Node* head;
+    Node* tail;
+  
+
+    Linkedlist(){
+
+        head=NULL;
+        tail=NULL;
+        
+    
+
+    }
+
+   void push_front(int val){
+            Node* newNode = new Node(val);    // for new node
+
+    if(head==NULL && tail==NULL){
+        tail=head=newNode;
+        }
+        else{
+            newNode->next=head;
+            newNode->prev=NULL;
+            head->prev=newNode;
+            head=newNode;
+        }
+
+    }
+
+
+
+
+    void push_back(int val){
+
+        Node* newNode = new Node(val); 
+
+    if(head==NULL && tail==NULL){
+        tail=head=newNode;
+        }
+        else{
+
+            tail->next = newNode;
+            newNode=NULL;
+            tail->prev=NULL;
+            tail=newNode; 
+
+        }
+
+
+
+
+    } 
+
+
+    void print_LL(){
+        Node* temp=head;
+        while(temp !=NULL){
+            cout<<temp->data<<"-> ";
+            temp=temp->next;
+        }
+        cout<<"NULL"<<endl;
+
+    
+    }
+
+
+};
+
+
+
+
+int main(){
+   
+   vector<int>nums;
+   doublylinkedlist ll;
+
+   ll.push_front(4);
+   ll.push_front(6);
+   ll.push_front(8);
+   ll.print_LL();       
+   
+
+}
---
+++ stack.cpp
@@ -0,0 +1,155 @@
+/*  #include<iostream>
+#include<vector>
+using namespace std;
+
+class stack{
+    public:
+    vector<int>v1;
+
+    void push(int val){
+        v1.push_back(val);
+    }
+
+    void pop(){
+        v1.pop_back();
+
+
+    }
+
+    void printstack(){
+        if(v1.size()==0){
+            cout<<"the stack is empty";
+
+        }
+        else{
+            for(int i=0;i<=v1.size()-1;i++){
+                cout<<v1[i];
+            }
+            
+        }
+
+        
+    }
+    
+
+
+
+};
+int main(){
+    stack obj;
+    obj.push(5);
+    obj.push(7);
+    obj.push(9);
+    
+    obj.printstack();
+
+    obj.pop();
+
+    
+} */
+
+
+/*  #include<iostream>                 // stack using linkedlist
+#include<list>
+using namespace std;
+
+class stack{
+    public:
+    list<int>l1;
+
+
+    void push(int val){
+        l1.push_front(val);
+
+    }
+
+    void pop(){
+        l1.pop_front();
+
+
+    }
+
+    void print(){
+        if(l1.size()==0){
+            cout<<"list is empty";
+        }
+        else{
+            for(int i=0;i<=l1.size()+1;i++){
+                cout<<l1.front();
+
+                l1.pop_front();
+
+
+            }
+            }
+            }
+        };
+int main(){
+
+    stack obj;
+    obj.push(5);
+    obj.push(4);
+    obj.push(3);
+
+
+    obj.print();
+
+
+}
+    */
+
+    #include<iostream>
+    #include<array>
+    using namespace std;
+
+    class stack{
+        public:
+        array<int,7>arr;
+        
+
+
+        void push(int val){                            //DRY  RUN PUSH CODE
+       int n=sizeof(arr)/sizeof(arr[0]);
+       if(n<=arr.size()){
+        int i=arr[arr.size()-1];
+
+        while(i<arr.size()){
+            arr[i]=arr[i+1];
+            i--;
+        }
+        arr[i]=val;
+
+       }
+       else{
+        cout<<"overflow-element can't be added"<<endl;
+       }
+    }
+
+    void pop(){
+
+    }
+
+    void print(){
+        if(arr.size()==0){
+            cout<<"stack is empty";
+        }
+        else{
+            for(int i=arr.size()-1;i>=0;i--){
+                cout<<arr[i];
+
+
+            }
+            }
+            }
+        };
+
+    int main(){
+        stack obj;
+        obj.push(5);
+        obj.push(6);
+        obj.push(7);
+
+        obj.print();
+
+
+    }
