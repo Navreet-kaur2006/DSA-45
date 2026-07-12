2a51a706e2c77da4ebe0d292faa611f68e045b19
navreet <navreetkaur732006@gmail.com>
2026-07-12 18:21:35 +0530

    queue

--- queue.cpp
+++ queue.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+ /* #include<iostream>                     //QUEUE
 using namespace std;
 
     class Node{
@@ -68,11 +68,132 @@ using namespace std;
 
     int main(){
         queue obj;
-        obj.push(5);
-        obj.push(7);
+        obj.push(4);
+        obj.push(8);
+        obj.push(6);
         obj.pop();
-
+        obj.pop();
         obj.print();
 
     }
+*/
 
+// double ended queue
+
+#include<iostream>
+using namespace std;
+
+//dequeue-> operation dequeue       deque-> doubly ended queue
+
+
+class Node{
+       public:
+       int data;
+       Node* next;
+
+       Node(int val){
+    data=val;
+    next=NULL;
+   }
+};
+
+
+class deque{
+
+    public:
+    Node* front=NULL;
+    Node* rear=NULL;
+
+    void enqueue(int val,int pos){
+        if(pos == 1){
+             Node* newNode =new Node(val);
+            if(front==NULL&& rear==NULL){
+                front = rear= newNode ; 
+            }else{
+                 Node* newNode =new Node(val);
+
+                newNode->next=front;
+            front=newNode;
+
+            }
+        }
+        else if(pos==-1){
+             Node* newNode =new Node(val);
+             if(front==NULL&& rear==NULL){
+                front = rear= newNode ; 
+            }else{
+                rear->next=newNode;
+            rear= newNode;
+
+            }
+       
+        }
+        else{
+            cout<<"pos is incorrect";
+        }
+    }
+
+    void dequeue(int pos){
+        if(pos==1){
+            if(front==NULL && rear==NULL){
+            cout<<"queue is empty";
+            }else{
+                Node* temp=front;
+
+            front=temp->next;
+            temp->next=NULL;
+           delete temp ;
+
+            }
+    
+        }
+        else if(pos==-1){
+            if(front ==NULL && rear == NULL ){
+                cout<<"Queue is Empty";
+            }else{
+            Node* temp=rear;
+            rear=temp->next;
+            temp->next=NULL;
+            delete temp;
+
+            }
+   
+            }
+
+        
+        else{
+            cout<<"pos is incorrect";
+        }
+
+    }
+
+    void print(){
+        if(front==NULL && rear==NULL){
+            cout<<"queue is empty";
+        }else{
+
+            Node* temp=front;
+            while(temp!= NULL){
+                cout<<temp->data<<"->";
+                temp=temp->next;
+            }
+
+            cout<<"NULL";
+
+        }
+
+            
+         }
+        
+        };
+
+int main(){
+
+    deque obj;
+    obj.enqueue(2,1) ; 
+    obj.enqueue(8,-1);
+    obj.print();
+
+    
+
+}
---
+++ queue2.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+class dequeue{
+    public:
+    queue<int>q1;
+
+    void push(int val){
+        q1.push(val);
+    }
+
+    void pop(){
+        q1.pop();
+    }
+
+    void print(){
+        if(q1.size()==0){
+            cout<<"queue is empty";
+        }
+
+        else{
+            while(!q1.empty()){
+                cout<<q1.front()<<" "; 
+                q1.pop();
+            }
+        }
+
+
+    }
+};
+int main(){
+    dequeue obj;
+    obj.push(5);
+    obj.push(7);
+    obj.push(8);
+
+   // obj.pop();
+
+    obj.print();
+}
