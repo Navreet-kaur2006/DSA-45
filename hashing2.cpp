ea1e920cace434f8098a16b4869cf71cc80f8ec8
navreet <navreetkaur732006@gmail.com>
2026-07-14 13:12:15 +0530

    hash

--- hashing2.cpp
+++ hashing2.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
 #include<list>
-using namespace std;                                 // hw:-1). searching in hashing  
+#include<vector>
+
+using namespace std;                                          // hw:-1). searching in hashing
+
+class hashtable{
+    public:
+    int size ; 
+    vector<list<int>> table ; 
+};
+
+
+class hashing{
+    public:
+    
+
+
+};
 
 int main(){
+
 
 
 
