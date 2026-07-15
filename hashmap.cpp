c77014432bc4abca6da54b1edd981ed86f3a21b4
navreet <navreetkaur732006@gmail.com>
2026-07-15 12:10:21 +0530

    hash

--- hashmap.cpp
+++ hashmap.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<iostream>                            // HASHMAP
 #include<map>
 using namespace std;
 int main(){
@@ -8,4 +8,17 @@ int main(){
     data[3]=4;
     data[4]=7;
 
+    map<string,int,greater<string>>marks;       // ,greater<string> for descending order
+    marks["naina"]=95;
+    marks["arya"]=92;
+    marks["vivan"]=86;
+    marks["avyan"]=94;
+
+   /* for(auto element:marks){                 // here marks is iterator          (auto)-bnya bnaya iterator a 
+        cout<<"Name-" <<element.first;
+        cout<<"  marks- "<<element.second<<endl;
+    }
+       */
+
+    map<string,int>value;
 }
