fa1acb064db5c1a4f322e804b4c083bcec927e9d
navreet <navreetkaur732006@gmail.com>
2026-07-15 11:38:53 +0530

    hash

--- hashing2.cpp
+++ hashing2.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/* #include<iostream>
 #include<list>
 #include<vector>
 
@@ -6,16 +6,31 @@ using namespace std;                                          // hw:-1). searchi
 
 class hashtable{
     public:
-    int size ; 
+    int tablesize ; 
     vector<list<int>> table ; 
-};
+
+     // constructor
+    hashtable(int size){
+
+        tablesize=size;
+        table.resize(tablesize);
+    }
+
+    void add(int val){
+        int key= val % tablesize;
+        table[key].push_back(val);                    //chaining
+        
 
 
-class hashing{
-    public:
-    
+    }
+    void del(int d){
+
+    }
+
+    void search(int s){
 
 
+    }
 };
 
 int main(){
@@ -24,4 +39,53 @@ int main(){
 
 
 
+}   */
+
+
+#include<iostream>                                //searchin in hashing
+using namespace std;
+
+int main(){
+    int size=10;
+    int hashtable[size];
+    for(int i=0;i<size;i++){
+        hashtable[i]=-1;
+    }
+
+    int n ; 
+    cout<<"enter the number of elements you want to add - "; 
+    cin>>n; 
+
+    for(int j=0;j<n;j++){  
+        int ele ;                           
+        cout<<"enter element: ";
+        cin>>ele;
+        int i = ele%size; 
+        while(hashtable[i]!=-1){                 
+            i= (ele + i )%size ; 
+            i= i++; 
+        }
+
+        hashtable[i]=ele;
+    }    
+
+    for(int k=0;k<size;k++){
+        cout<<hashtable[k]<<"->";
+
+    }
+   int target;                                // searching code
+   cout<<"enter target element:";
+   cin>>target;
+   bool res;
+   for(int s=0;s<size;s++){
+    if(hashtable[s]==target){
+        res=true;
+    }
+   }
+   if(res==true){
+    cout<<"element is found";
+   }
+   else{
+    cout<<"element not found";
+   }
 }
---
+++ hashmap.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<map>
+using namespace std;
+int main(){
+    map<int,int>data;
+    data[1]=1;
+    data[2]=3;
+    data[3]=4;
+    data[4]=7;
+
+}
